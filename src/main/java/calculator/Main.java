0d64d9e46cfb46bb30215132be467bff3b4f4d6e
rakeshkumarbiswal <42043175+rakeshkumarbiswal@users.noreply.github.com>
2020-07-08 09:46:56 +0530

    Update Main.java

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String args[])
     {
+        int p;
         int flag=0,ch;
         double num1,num2;
         Scanner reader = new Scanner(System.in);
